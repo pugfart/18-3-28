31a76da80d6ea4a3e168d2241a0d30a26826d4bc
Andrew Hua (花毓禧) <andrew.hua@pomerobotservice.com>
2018-03-28 17:40:57 +0800

    下班前粗製版

---
+++ ConsoleApplication3/ConsoleApplication3/People.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class People
+    {
+        public People()
+        {
+
+        }
+
+        public People(string name, int hp)
+        {
+            this.name = name;
+            this.hp = hp;
+        }
+
+        public string name;
+
+        public int hp;
+
+        public int attack(People p,int a)
+        {
+            p.hp = p.hp - a;            
+            if (p.hp < 0) p.hp = 0;
+            return p.hp;
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People grace = new People("grace", 100);
+            People andrew = new People("andrew", 100);
+
+            Random a = new Random();
+            int hurt,dead=1;
+            while (grace.hp >= 1 && andrew.hp >= 1)
+            {
+                hurt=a.Next(5);
+                int hp = grace.attack(andrew,hurt);
+                Console.WriteLine(andrew.name + "被攻擊了, 血量少了"+hurt+"滴, hp = " + hp);
+                if (andrew.hp < 1) { dead = 0; break; }
+                hurt = a.Next(5);
+                hp = andrew.attack(grace,hurt);
+                Console.WriteLine(grace.name + "被攻擊了, 血量少了" + hurt + "滴, hp = " + hp);                
+            }
+            if (dead == 1)
+                Console.WriteLine("\n"+grace.name + "葛屁");
+            else
+                Console.WriteLine("\n"+       andrew.name + "葛屁");
+            Console.Read();
+        }
+    }
+}
