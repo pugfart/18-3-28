792ba9240500bff0dbaa28f524d213c772141e75
Grace Huang (黃思嘉) <grace.huang@pomerobotservice.com>
2018-03-29 16:44:31 +0800

    sword damage completed

---
+++ ConsoleApplication3/ConsoleApplication3/sword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class sword
+    {
+        public int damage;
+        public sword()
+        {
+            Random b = new Random();
+            damage =b.Next(1,4);
+        }       
+    }
+}
