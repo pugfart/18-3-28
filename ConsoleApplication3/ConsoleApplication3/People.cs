128a8169c8cb44c8024db87ba484104ca93e0490
Andrew Hua (花毓禧) <andrew.hua@pomerobotservice.com>
2018-03-29 14:01:47 +0800

    加入迴避 攻擊力不同

--- ConsoleApplication3/ConsoleApplication3/People.cs
+++ ConsoleApplication3/ConsoleApplication3/People.cs
@@ -23,11 +23,22 @@ namespace ConsoleApplication3
 
         public int hp;
 
+        public int avoid;
+
+        public int hit;
+
+        public void setting(int a,int h)
+        {
+            this.avoid = a;
+            this.hit = h;
+        }
+
         public int attack(People p,int a)
         {
-            p.hp = p.hp - a;            
-            if (p.hp < 0) p.hp = 0;
-            return p.hp;
+                p.hp = p.hp - a;
+                if (p.hp < 0) p.hp = 0;
+                return p.hp;
+            
         }
     }
 }
--- ConsoleApplication3/ConsoleApplication3/Program.cs
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -13,17 +13,42 @@ namespace ConsoleApplication3
             People grace = new People("grace", 100);
             People andrew = new People("andrew", 100);
 
+            grace.setting(20, 5);
+            andrew.setting(10, 6);
+
             Random a = new Random();
-            int hurt,dead=1;
+            int hurt,dead=1,hp;
+            bool pouch;
+
             while (grace.hp >= 1 && andrew.hp >= 1)
             {
-                hurt=a.Next(5);
-                int hp = grace.attack(andrew,hurt);
-                Console.WriteLine(andrew.name + "被攻擊了, 血量少了"+hurt+"滴, hp = " + hp);
-                if (andrew.hp < 1) { dead = 0; break; }
-                hurt = a.Next(5);
-                hp = andrew.attack(grace,hurt);
-                Console.WriteLine(grace.name + "被攻擊了, 血量少了" + hurt + "滴, hp = " + hp);                
+                hurt=a.Next(1,grace.hit);
+                pouch = andrew.avoid < a.Next(100);
+                if (pouch)
+                {
+                    hp = grace.attack(andrew, hurt);
+                    Console.WriteLine(andrew.name + "被攻擊了, 血量少了" + hurt + "滴, hp = " + hp);
+                }
+                else
+                    Console.WriteLine(andrew.name + "躲開了!!!");
+                System.Threading.Thread.Sleep(100);
+
+                if (andrew.hp < 1)
+                {
+                    dead = 0;
+                    break;
+                }
+
+                hurt = a.Next(1,andrew.hit);
+                pouch = grace.avoid < a.Next(100);
+                if (pouch)
+                {
+                    hp = andrew.attack(grace, hurt);
+                    Console.WriteLine(grace.name + "被攻擊了, 血量少了" + hurt + "滴, hp = " + hp);
+                }
+                else
+                    Console.WriteLine(grace.name + "躲開了!!!");
+                System.Threading.Thread.Sleep(100);              
             }
             if (dead == 1)
                 Console.WriteLine("\n"+grace.name + "葛屁");
